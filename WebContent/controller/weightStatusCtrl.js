34b6d1cfb3650f8b25490aae3ee725ee004b6081
AarthiRT <aaarthirt@gmail.com>
2017-06-14 10:54:36 +0530

    Add files via upload

--- WebContent/controller/weightStatusCtrl.js
+++ WebContent/controller/weightStatusCtrl.js
@@ -1,31 +1,16 @@
-var graphController = healthStatusApp.controller(
+var graphController = weightStatusApp.controller(
 		"graphCtrl",
 		function($scope) {
-			$scope.labels = ['10','20','30','40','50','60','70','80','90','100','110','120'];
-			$scope.series = [ 'Today', 'Avg' ];
-
-			$scope.data = [ [ 60, 30, 20, 35, 40, 38, 50, 70, 50, 20, 30, 40 ],
-			                [ 15, 12, 10, 15, 20, 18, 15, 16, 12, 25, 35, 30 ] ];
-			$scope.colors = [ '#46EEE1','#DCDCDC'];
+			$scope.labels = ['','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP'];
+			$scope.data = [ [ 100, 95, 90, 95, 92, 90, 85, 88, 84]];
+			$scope.colors = [ '#46EEE1'];
 			$scope.options = {
-					/*title: {
-						display: true,
-						position:'top',
-						fontFamily:'Gill Sans Extrabold, sans-serif',
-						fontColor: '#A0A0A0',
-						text: 'Moving Activity'
-					},*/
-					legend: {
-						display: true,
-						position:'right',
-						labels : {
-							fontColor: '#A0A0A0'
-						}
-					},
-					elements: { point: { radius: 0 } },
+					elements: { point: { radius: 0 },
+								line: { tension: 0 }},
 					scales : {
 						xAxes : [ {
-							display:false,
+							display:true,
+							position:'top',
 							gridLines : {
 								color : "rgba(0, 0, 0, 0)",
 							}
@@ -41,81 +26,3 @@ var graphController = healthStatusApp.controller(
 
 		});
 
-
-var tempGraphController = healthStatusApp.controller(
-		"tempGraph",
-		function($scope) {
-			$scope.labels = ['10','20','30','40','50','60','70','80','90','100','110','120'];
-			$scope.data = [ [ 60, 30, 20, 35, 40, 38, 50, 70, 50, 20, 30, 40 ] ];
-			$scope.colors = [{backgroundColor:"rgba(0, 0, 0, 0)" ,borderColor: '#FF5733'}];
-			$scope.options = {
-					elements: { point: { radius: 0 } },
-					scales : {
-						xAxes : [ {
-							display:false,
-							gridLines : {
-								color : "rgba(0, 0, 0, 0)",
-							}
-						} ],
-						yAxes : [ {
-							display:false,
-							gridLines : {
-								color : "rgba(0, 0, 0, 0)",
-							}
-						} ],
-					}			        
-			};
-
-		});
-
-var tempGraphController = healthStatusApp.controller(
-		"calGraph",
-		function($scope) {
-			$scope.labels = ['10','20','30','40','50','60','70','80','90','100','110','120'];
-			$scope.data = [ [  50, 70, 50, 20, 30, 40, 60, 30, 20, 35, 40, 38 ] ];
-			$scope.colors = [{backgroundColor:"rgba(0, 0, 0, 0)" ,borderColor: '#22DCEE'}];
-			$scope.options = {
-					elements: { point: { radius: 0 } },
-					scales : {
-						xAxes : [ {
-							display:false,
-							gridLines : {
-								color : "rgba(0, 0, 0, 0)",
-							}
-						} ],
-						yAxes : [ {
-							display:false,
-							gridLines : {
-								color : "rgba(0, 0, 0, 0)",
-							}
-						} ],
-					}			        
-			};
-
-		});
-
-var tempGraphController = healthStatusApp.controller(
-		"heartGraph",
-		function($scope) {
-			$scope.labels = ['10','20','30','40','50','60','70','80','90','100','110','120'];
-			$scope.data = [ [  50, 20, 50, 20, 50, 20, 50, 20, 50, 20, 50, 20 ] ];
-			$scope.colors = [{backgroundColor:"rgba(0, 0, 0, 0)" ,borderColor: '#B513C8'}];
-			$scope.options = {
-					elements: { point: { radius: 0 } },
-					scales : {
-						xAxes : [ {
-							display:false,
-							gridLines : {
-								color : "rgba(0, 0, 0, 0)",
-							}
-						} ],
-						yAxes : [ {
-							display:false,
-							gridLines : {
-								color : "rgba(0, 0, 0, 0)",
-							}
-						} ],
-					}			        
-			};
-
-		});
